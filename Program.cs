672508b42bf24c32234b15ce2b2f112340569fd8
Jorch34 <jorchgt34@gmail.com>
2025-04-26 22:27:09 -0600

    Escuela demo con uso de clases terminado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int fechaFundacion;
+        public string ceo = "Freddy Vega";
+
+        public void Timbrar() 
+        {
+            //ToDo: Implementar el timbre
+            //Console.Beep(10000, 3000); Solo funciona en Windows
+            Console.WriteLine("Timbrando....");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Platzi Academy";
+            miEscuela.direccion = "Cr 9 calle 72";
+            miEscuela.fechaFundacion = 2012;
+            miEscuela.ceo = "Otro Freddy Vega";
+            //miEscuela.Timbrar(); Solo funciona en Windows
+        }
+    }
+}
